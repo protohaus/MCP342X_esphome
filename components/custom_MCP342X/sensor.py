72d633d27712ab88ad5317bb7060de977c53a8c4
Tobias Klaus <tobias.klaus@protohaus.org>
2022-06-20 17:29:20 +0200

    basic esphome sensor setup

--- components/custom_MCP342X/sensor.py
+++ components/custom_MCP342X/sensor.py
@@ -1,16 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, sensor
+from esphome.components import sensor
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@proto"]
 
-DEPENDENCIES = ["i2c"]
-
 custom_mcp_ns = cg.esphome_ns.namespace("mcp")
 
 CustomMCP342XSensor = custom_mcp_ns.class_(
-    "CustomMCP342XSensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
+    "CustomMCP342XSensor", sensor.Sensor, cg.PollingComponent
 )
 
 CONFIG_SCHEMA = (
@@ -20,7 +18,6 @@ CONFIG_SCHEMA = (
         }
     )
     .extend(cv.polling_component_schema("1s"))
-    .extend(i2c.i2c_device_schema(None))
 )
 
 
@@ -28,4 +25,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
-    await i2c.register_i2c_device(var, config)
